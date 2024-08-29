c142e4a0646fe51126220a7f5ae76e7551dcd21c
Bibhu <bibhushitregmi0@gmail.com>
2024-08-29 17:41:27 -0400

    Simple program to check the plaindormic string

---
+++ String/palindrome.java
@@ -0,0 +1,34 @@
+package String;
+
+public class palindrome {
+
+    public static void main(String[] args) {
+        String str = "abcdcba";
+        System.out.println(ispalindrome(str));
+
+    }
+
+    static boolean ispalindrome(String str) {
+        str = str.toLowerCase();
+
+        char s = str.charAt(0);
+        char e = str.charAt(str.length() - 1);
+        while (s < e) {
+            if (s != e) {
+                return false;
+            } else {
+                s++;
+                e--;
+            }
+        }
+        return true;
+
+    }
+}
+
+/*
+ * Using two pointer method to check whether the string is palindorme or not.
+ * First pointer starts at the index 0 of the stirng and last pointer at the end
+ * and if the condition satisfies it returns true else the points moves forward
+ * and backward respectively.
+ */
