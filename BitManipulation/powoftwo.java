8cb75ad6713417f5df825a4d43f7725cfc0a6571
Bibhu <bibhushitregmi0@gmail.com>
2024-09-03 14:37:25 -0400

    Check if the number is a power of 2.

---
+++ BitManipulation/powoftwo.java
@@ -0,0 +1,27 @@
+package BitManipulation;
+
+public class powoftwo {
+
+    public static void main(String[] args) {
+
+        int n = 8;
+        boolean ans = (n & (n - 1)) == 0;
+        System.out.println(ans);
+    }
+}
+
+/*
+ * 8 -> 1000
+ * 1000 & (1000 -1) -> 1000 & 0111 -> 0000 == 0
+ * 
+ * 16 -> 10000
+ * 10000 & (10000 -1) -> 10000 & 01111 -> 00000 == 0
+ * 
+ * There is a pattern in binary that the all the power of 2 has only
+ * one setbit '1' and rest of them 0.
+ * 
+ * 2 -> 10
+ * 4 -> 100
+ * 8 -> 1000
+ * 16 -> 10000 an so on for every power of 2.
+ */
