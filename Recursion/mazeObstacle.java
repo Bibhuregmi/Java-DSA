1a61e88b69f48a014be3b7cabe908f11e95c4d86
Bibhu Regmi <bibhushitregmi0@gmail.com>
2024-09-14 14:41:55 -0400

    Can move to all direction. Causes the stack overflow problem

--- Recursion/mazeObstacle.java
+++ Recursion/mazeObstacle.java
@@ -27,5 +27,17 @@ public class mazeObstacle {
         if(r < arr.length -1 && c < arr[0].length-1 ){ //Condition to move down diagoanlly 
             obstacles(e + "di", arr, r+1, c+1);
         }
+            // Now we are given the choice that we can move into any directions be that L, R, U, D and di. so for that I will introduce two 
+            // conditions.
+        if( r > 0){
+            obstacles(e+ "U", arr, r-1, c);
+        } 
+        if (c > 0){
+            obstacles(e + "L", arr, r, c-1);
+        }
+
+        //  After running this program, it goes to stack overflow because program will keep going to the starting position and base case will never hit. 
+
+    
     }
 }
