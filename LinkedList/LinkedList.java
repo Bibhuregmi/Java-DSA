eb1f33105d86799fb48bef5ead0ec73618df5ee6
Bibhu Regmi <bibhushitregmi0@gmail.com>
2024-10-03 18:44:21 -0400

    Added the method for the deletion in Singly Linked List

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -29,9 +29,9 @@ public class LinkedList {
             insert(val);
             return;
         }
-        Node node = new Node(val);
-        tail.next = node;
-        tail = node; 
+        Node node = new Node(val); //Creating new node with the value 
+        tail.next = node; //Pointing the tail to the new node
+        tail = node; //new node becomes tail
         size++;
     }
 
@@ -45,14 +45,70 @@ public class LinkedList {
             return; 
         }
 
-        Node temp = head;
-        for(int i =0; i< index-1; i++){
-            temp = temp.next; 
+        Node temp = head; //Starting form the head 
+        for(int i =0; i< index-1; i++){ 
+            temp = temp.next; //move to the next node in the list 
         }
 
-        Node node = new Node(val, temp.next);
-        temp.next = node;
+        Node node = new Node(val, temp.next); //Creating new node with the value and it is pointing to the temp.next which is the node at the index+1 position
+        temp.next = node; //now the node at index-1 points to the new node. 
         size++;
+    }
+
+    public int deleteFirst(){
+        int val = head.value; //getting the value of the head pointer
+        head = head.next; //updating the head to point to the next node, the value is removed cause there will be no node referencing it
+        if (head == null){ //if there is only one node
+            tail = null; //tail will be updated to point to null and the list becomes empty. 
+        }
+        size--;
+        return val;
+    }
+
+    public Node get(int index){
+        Node node = head; //Starting from the head node 
+        for(int i=0; i<index; i++){
+            node = node.next; //traverse through the list till reached the index 
+        }
+        return node; //returns the node at the specified index
+    }
+
+    public Node find (int value){
+        Node node = head;
+        while (node != null){
+            if (node.value == value){
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public int deleteLast(){
+        if (size <= 1){ //if there is only one node in the list
+            return deleteFirst();
+        }
+
+        Node secondLast = get(size -2); //accessing the node that is just before the last node 
+        int val = tail.value; // storing the value of the current tail node before removing it
+        tail = secondLast; //tail pointer is updated to point towards the node that is before the last node
+        tail.next = null; // the new last node will now points towards null ensuring the previous last node is not referenced by any other nodes
+        size--;
+        return val;
+    }
+
+    public int delete(int index){
+        if (index == 0){ 
+            return deleteFirst();
+        }
+        if (index == size -1){
+            return deleteLast();
+        }
+
+        Node prev = get (index -1); //getting the node just before the given index
+        int val = prev.next.value; //storing the value of the node that is going to be removed
+        prev.next = prev.next.next; // updating the pointer of the node before the index to point the the node after the index so that the node at the index have no any other node that will reference it. 
+        return val;
     }
     /*
      * To display the values present in the linked list, we need to create a temp node which points toward
@@ -67,13 +123,10 @@ public class LinkedList {
         }
         System.out.println("null");
     }
-    
-}
-
-/*
+    /*
  * This class represents each node in linked list which contain value and the reference to the next node
 */
- class Node{ 
+ private class Node{ //{Changed the Node class to the private inner class so that no other external classes can interact directly to the Node class}
     int value; //data stored in the node
     Node next; // reference to the next node
 
@@ -85,4 +138,12 @@ public class LinkedList {
         this.value = value;
         this.next = next; 
     }
+
+    //Override the toString method to display the meaningful output for the find() method
+    @Override
+    public String toString(){
+        return "Node{" + "vaule=" + value + "}";
+    }
+}
+    
 }
