e2770a5a079ebfb35e67b1cde126b62bb74b8c85
Bibhu Regmi <bibhushitregmi0@gmail.com>
2024-10-15 13:49:08 -0400

    Refactored the code for correct handling of the (size)

--- stacksNqueues/circularQueue.java
+++ stacksNqueues/circularQueue.java
@@ -5,31 +5,31 @@ class circularQueue {
     private static final int DEFAULT_SIZE = 10;  //Default queue size
 
     protected int end, front, size = 0; //pointers to track front and end positions and the size of the queue
-
+    private int capacity; 
     public circularQueue(){ //default constructor with predefined size
         this(DEFAULT_SIZE); 
     }
 
     public circularQueue(int size){ //constructs the queue with the given size
         this.data = new int[size]; 
-        this.size = size; 
+        this.capacity = size; 
     }
 
     public boolean isFull(){ //checks if the queue is full
-        return (end +1) % size == front; 
+        return size == capacity; 
     }
     public boolean isEmpty(){ //checks if the queue is empty
-        return front == end; 
+        return size == 0; 
     }
 
     public boolean insert(int item){ //insertion in queue
         if (isFull()){
             System.out.println("Queue is full");
             return false; 
-        }else{
-            data[end] = item; 
-            end = (end +1) % size; //circular increment of the end pointer
         }
+        data[end] = item; 
+        end = (end +1) % capacity; //circular increment of the end pointer
+        size++;
         return true; 
     }
 
@@ -38,7 +38,8 @@ class circularQueue {
             throw new Exception("Queue is empty"); 
         }
         int item = data[front]; //geting the item form the front of the queue
-        front = (front +1) % size; //circular increment of the front pointer
+        front = (front +1) % capacity;//circular increment of the front pointer
+        size--; 
         return item; 
     }
 
@@ -48,25 +49,26 @@ class circularQueue {
             return; 
         }
         int i = front; //starting from front
-        while(true){
+        int element = size; 
+        while(element > 0){
             System.out.print(data[i] + " ");
-            if (i == end){ //stops when i reaches to end
-                break; 
-            }
-            i = (i+1)%size; //circular increment of the index
+            i = (i+1)%capacity; //circular increment of the index
+            element--; 
         }
         System.out.println();
     }
     public static void main(String[] args) throws Exception {
         circularQueue queue = new circularQueue(); 
         queue.insert(6);
-        queue.insert(0);
+        queue.insert(4);
         queue.insert(7);
         queue.insert(10);
         queue.insert(2);
         queue.insert(8);
         queue.display();
        System.out.println(queue.remove());
+       queue.display();
+       queue.insert(88);
        queue.display();
 
     }
